3d83d23347e56bf0e54dc137f6f74b76b0c5923d
race604 <race604@gmail.com>
2022-06-12 13:49:38 +0800

    add unit test for Cache

--- src/cache.rs
+++ src/cache.rs
@@ -38,7 +38,6 @@ pub struct Cache {
     disk: Option<HashTableOwned<OdhtConfig>>,
     memo_limit: usize,
     memo_size: usize,
-    disk_used: bool,
 }
 
 impl Cache {
@@ -52,26 +51,37 @@ impl Cache {
                 memo_limit
             },
             memo_size: 0,
-            disk_used: false,
         }
     }
 
     pub fn insert(&mut self, item: &str) -> bool {
+        if self.memo_size >= self.memo_limit {
+            debug!("Memory cache is full, dump to disk");
+            self.dump_to_disk();
+        }
+
         let mut res = self.memo.insert(item.to_owned());
         if res {
             self.memo_size = self.memo_size + item.len();
-            if self.disk_used {
+            if self.disk.is_some() {
                 res = self.insert_on_disk(item);
                 debug!("Insert on disk: {}", res);
-            }
-
-            if self.memo_size >= self.memo_limit {
-                debug!("Memory cache is full, dump to disk");
-                self.dump_to_disk();
             }
         }
 
         res
+    }
+
+    pub fn contains(&self, item: &str) -> bool {
+        if self.memo.contains(item) {
+            return true;
+        }
+
+        return if let Some(ref disk) = self.disk {
+            Cache::item_to_keys(item).all(|key| disk.contains_key(&key))
+        } else {
+            false
+        };
     }
 
     fn insert_on_disk(&mut self, item: &str) -> bool {
@@ -80,21 +90,69 @@ impl Cache {
             HashTableOwned::<OdhtConfig>::with_capacity(1_000_000, 95)
         });
         let mut res = false;
-        for (i, chunk) in item.as_bytes().chunks(CHUNK_SIZE).into_iter().enumerate() {
-            let mut key = [0 as u8; CHUNK_SIZE + 1];
-            key[CHUNK_SIZE] = i as u8;
-            key[..chunk.len()].copy_from_slice(chunk);
+        for key in Cache::item_to_keys(item) {
             res = disk.insert(&key, &true).is_none() || res;
         }
         res
     }
 
+    fn item_to_keys<'a>(item: &'a str) -> impl Iterator<Item = [u8; CHUNK_SIZE + 1]> + 'a {
+        let res = item
+            .as_bytes()
+            .chunks(CHUNK_SIZE)
+            .into_iter()
+            .enumerate()
+            .map(|(i, chunk)| {
+                let mut key = [0 as u8; CHUNK_SIZE + 1];
+                key[CHUNK_SIZE] = i as u8;
+                key[..chunk.len()].copy_from_slice(chunk);
+                key
+            });
+        return res;
+    }
+
     fn dump_to_disk(&mut self) {
-        self.disk_used = true;
         let keys = self.memo.drain().collect::<Vec<_>>();
         for key in keys {
             self.insert_on_disk(&key);
         }
         self.memo_size = 0;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::distributions::Alphanumeric;
+    use rand::{thread_rng, Rng};
+
+    #[test]
+    fn test_basic_cache() {
+        let mut cache = Cache::new(0);
+        assert!(cache.insert("hello"));
+        assert!(cache.insert("world"));
+
+        assert!(cache.contains("hello"));
+        assert!(cache.contains("world"));
+        assert!(!cache.contains("other"));
+    }
+
+    #[test]
+    fn test_limit_memory() {
+        let mut cache = Cache::new(1024);
+        for _ in 0..100 {
+            cache.insert(&rand_string(32));
+        }
+        assert!(cache.memo.len() < 100);
+        assert!(cache.disk.is_some());
+        assert!(cache.disk.unwrap().len() > 0);
+    }
+
+    fn rand_string(len: usize) -> String {
+        thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(len)
+            .map(char::from)
+            .collect()
     }
 }
