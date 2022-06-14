7b261521963534bd876551b592cfb15e494aeea8
race604 <race604@gmail.com>
2022-06-14 22:18:30 +0800

    bump clap version

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,15 @@ use clap::Parser;
 #[clap(author, about, version, long_about = None)]
 pub struct Cli {
     /// Input file name
+    #[clap(value_parser, value_name = "INPUT FILE")]
     pub input: Option<PathBuf>,
 
     /// Output file name
+    #[clap(value_parser, value_name = "OUTPUT FILE")]
     pub output: Option<PathBuf>,
 
     /// Memory limit in bytes, 0 for unlimited
-    #[clap(short, long, default_value_t = 0)]
+    #[clap(short, long, action = clap::ArgAction::Set, default_value_t = 0)]
     pub memo_limit: usize,
 
     /// Whether to print verbose output
